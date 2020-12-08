c6914d280bc89093760cae38ad550f91b9c88d50
LAzarrr22 <maciek.janik24@gmail.com>
2020-12-08 18:14:34 +0100

    refactor posts controller

--- src/main/java/com/MJ/MotoFreaksBackend/MotoFreaksBackend/resource/controller/PostsController.java
+++ src/main/java/com/MJ/MotoFreaksBackend/MotoFreaksBackend/resource/controller/PostsController.java
@@ -26,47 +26,47 @@ public class PostsController {
         this.postsService = postsService;
     }
 
-    @RequestMapping(path = "/get/ALL", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(path = "/ALL", method = RequestMethod.GET, produces = "application/json")
     public Object getAll(@RequestParam Map<String, String> reqParams) {
         return postsService.getPosts(null,reqParams,null);
     }
 
-    @RequestMapping(path = "/get/{type}", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(path = "/{type}", method = RequestMethod.GET, produces = "application/json")
     public Object getAllByType(@PathVariable PostType type, @RequestParam Map<String, String> reqParams) {
         return postsService.getPosts(type, reqParams,null);
     }
-    @RequestMapping(path = "/creator/id/{id}/get/ALL", method = RequestMethod.GET, produces = "application/json")
-    public Object getPostsById(@PathVariable String id, @RequestParam Map<String, String> reqParams) {
+    @RequestMapping(path = "/byUser/{id}/ALL", method = RequestMethod.GET, produces = "application/json")
+    public Object getPostsByUser(@PathVariable String id, @RequestParam Map<String, String> reqParams) {
         return postsService.getPosts(null, reqParams, id);
     }
-    @RequestMapping(path = "/creator/id/{id}/get/{type}", method = RequestMethod.GET, produces = "application/json")
-    public Object getPostsByIAndType(@PathVariable String id, @PathVariable PostType type, @RequestParam Map<String, String> reqParams) {
+    @RequestMapping(path = "/byUser/{id}/{type}", method = RequestMethod.GET, produces = "application/json")
+    public Object getPostsByUserAndType(@PathVariable String id, @PathVariable PostType type, @RequestParam Map<String, String> reqParams) {
         return postsService.getPosts(type, reqParams, id);
     }
 
-    @RequestMapping(path = "/add", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(path = "", method = RequestMethod.PUT, produces = "application/json")
     public Object addPost(HttpServletRequest req, @RequestBody NewPost newPost) {
         String token = req.getHeader(AuthorizationHeader.HEADER_NAME).replace(AuthorizationHeader.TOKEN_PREFIX, "");
         return postsService.addPost(newPost, token);
     }
 
-    @RequestMapping(path = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
-    public Object deleteMyPost(@PathVariable String id) {
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public Object deletePost(@PathVariable String id) {
         return postsService.deletePost(id);
     }
 
     @RequestMapping(path = "/resolve/{id}", method = RequestMethod.POST, produces = "application/json")
-    public Object resolveMyPosts(@PathVariable String id) {
+    public Object resolvePost(@PathVariable String id) {
         return postsService.resolvePost(id);
     }
 
-    @RequestMapping(path = "/{postId}/add/comment", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(path = "/{postId}/comment", method = RequestMethod.PUT, produces = "application/json")
     public Object addComment(HttpServletRequest req, @PathVariable String postId, @RequestBody NewMessage newComment) {
         String token = req.getHeader(AuthorizationHeader.HEADER_NAME).replace(AuthorizationHeader.TOKEN_PREFIX, "");
         return postsService.addComment(token, postId, newComment);
     }
 
-    @RequestMapping(path = "/{postId}/delete/comment/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    @RequestMapping(path = "/{postId}/comment/{id}", method = RequestMethod.DELETE, produces = "application/json")
     public Object deleteComment(@PathVariable String postId,  @PathVariable String id) {
         return postsService.deleteComment(postId, id);
     }
